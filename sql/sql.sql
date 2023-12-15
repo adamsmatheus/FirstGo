cd29e96a0cd7be745b4f22fdc43b029c28582b41
Matheus Adams <62621727+adamsmatheus@users.noreply.github.com>
2023-12-14 23:56:51 -0300

    create table SQL

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,13 +1,16 @@
-CREATE DATABASE IF NOT EXISTS appgo;
-USE appgo;
+CREATE
+DATABASE IF NOT EXISTS appgo;
+USE
+appgo;
 
 DROP TABLE IF EXISTS usuarios;
 
-CREATE TABLE usuarios(
-    id int auto_increment primary key,
-    nome varchar(50) not null,
-    nick varchar(50) not null unique,
-    email varchar(50) not null unique,
-    senha varchar(50) not null unique,
+CREATE TABLE usuarios
+(
+    id       int auto_increment primary key,
+    nome     varchar(50) not null,
+    nick     varchar(50) not null unique,
+    email    varchar(50) not null unique,
+    senha    varchar(50) not null unique,
     createAt timestamp default current_timestamp()
-)ENGINE=INNODB
+) ENGINE=INNODB
