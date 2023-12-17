923ca0bd4d3b2e0f0cf9dc7309a577b4f46eb906
Matheus Adams <62621727+adamsmatheus@users.noreply.github.com>
2023-12-16 22:19:53 -0300

    config url

--- config/config.go
+++ config/config.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
+	"os"
+	"strconv"
 )
 
 var (
@@ -16,4 +19,13 @@ func Carregar() {
 	if erro = godotenv.Load(); erro != nil {
 		log.Fatal(erro)
 	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+	StringConexao = fmt.Sprintf("connectionString := fmt.Sprintf(\"%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NOME"))
 }
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"GoTest/config"
 	"GoTest/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
+
+	println(config.Porta)
+	println(config.StringConexao)
 
 	r := router.Gerar()
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
