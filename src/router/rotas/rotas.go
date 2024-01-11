10d001050f6df061a5e893518ae5214937b61d7a
Matheus Adams <62621727+adamsmatheus@users.noreply.github.com>
2024-01-11 00:44:43 -0300

    Implementacao do metodo de login e criacao de rota

--- src/router/rotas/rotas.go
+++ src/router/rotas/rotas.go
@@ -16,7 +16,7 @@ type Rota struct {
 // Configura todas as rotas
 func Configurar(r *mux.Router) *mux.Router {
 	rotas := rotasUsuarios
-
+	rotas = append(rotas, rotaLogin)
 	for _, rota := range rotas {
 		r.HandleFunc(rota.Uri, rota.Funcao).Methods(rota.Metodo)
 	}
