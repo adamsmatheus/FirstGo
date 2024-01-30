b4c76d99d65d736d5f2224d94b5a830bc623c2a3
Matheus Adams <62621727+adamsmatheus@users.noreply.github.com>
2024-01-30 00:23:05 -0300

    Implementacao do Token de validacao

--- src/autenticacao/token.go
+++ src/autenticacao/token.go
@@ -3,6 +3,8 @@ package autenticacao
 import (
 	"GoTest/src/config"
 	"github.com/dgrijalva/jwt-go"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -14,4 +16,20 @@ func CriarToken(usuarioID uint64) (string, error) {
 	permissoes["usuarioId"] = usuarioID
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString(config.SecretKey)
+}
+
+func ValidarToken(r *http.Request) error {
+	tokenString := ExtrairToken(r)
+	token, erro := jwt.Parse(tokenString)
+}
+
+func ExtrairToken(r *http.Request) string {
+
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return " "
 }
